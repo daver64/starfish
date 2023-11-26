e82fa5eaa14e94790e03f7a79e7e0ad8c62501a1
David Rowbotham <thedaver64@gmail.com>
2023-11-26 06:01:49 +0000

    added predefined colours ( x11, html etc )

--- src/starfish.cpp
+++ src/starfish.cpp
@@ -18,18 +18,20 @@ int main(int argc, char *argv[])
     int32 result=sl_create_context(&context,"simlib app",screen_w,screen_h,fullscreen);
     
     tex=new SLTexture(256,256,true);
+    geometry=new SLPrimitiveBuffer;
 
-    geometry=new SLPrimitiveBuffer(GL_TRIANGLES,GL_DYNAMIC_DRAW);
     sl_disable_depthtest();
     sl_disable_texturing();
+
     do {
         sl_process_input(context);
         sl_ortho(context);
-        sl_clrscr(context,rgb(100,0,0));
-        sl_trianglefill(geometry,10,100,100,10,200,100,argb(200,200,0,255));
+        sl_clrscr(context);
+        sl_trianglefill(geometry,10,100,100,10,200,100,x11colours::silver);
         sl_swap(context);
         sl_poll_input(context);
     } while(!sl_want_to_quit(context));
+
     sl_destroy_context(&context);   
     delete tex;
     delete geometry;
